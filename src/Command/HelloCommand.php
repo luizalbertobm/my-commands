64a930ed0df1c829ff39182292c7d8f19af6b8a2
Luiz Mesquita <luizalbertobm@gmail.com>
2025-04-21 00:38:21 +0100

    feat(console): add Symfony console command structure

---
+++ src/Command/HelloCommand.php
@@ -0,0 +1,26 @@
+<?php
+namespace MyCommands\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'hello',
+    description: 'Exibe uma saudação personalizada.',
+)]
+class HelloCommand extends Command
+{
+
+    protected function configure(): void
+    {
+        $this->setHelp('Este comando permite que você receba uma saudação na linha de comando.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Olá, Luiz Alberto! Este é seu comando Symfony Console.');
+        return Command::SUCCESS;
+    }
+}
